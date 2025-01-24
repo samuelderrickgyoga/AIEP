9a479abca9a0d580473489da60506b895333cb97
samuelderrickgyoga <samuelderrickgyoga@gmail.com>
2025-01-24 15:34:27 +0300

    configured postgres

---
+++ backend/config.py
@@ -0,0 +1,4 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'postgresql://myuser:mypassword@localhost/learning_platform'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = 'derrickjwt003@phoenix' 
