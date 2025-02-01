751e0940aab87e51d947402af4031bcb8155b29b
samuelderrickgyoga <samuelderrickgyoga@gmail.com>
2025-02-01 08:54:13 +0300

    env

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,9 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://myuser:mypassword@localhost/learning_platform'
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = 'derrickjwt003@phoenix' 
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET")
